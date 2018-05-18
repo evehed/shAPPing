66a94c585d46c84d0edf08bb2665c366cc14148b
lucasahlgren <35539195+lucasahlgren@users.noreply.github.com>
2018-05-18 12:22:49 +0200

    eve sign up

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 // Initialize Firebase
 var config = {
 	apiKey: "AIzaSyDFN8hxCYuOipmB-UWY0JsQPICV6jSqxHM",
@@ -39,66 +38,6 @@ function signUpPage(){
 	signUpElement.style.display = "block";
 	navBarElement.style.display = "none";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-// Logout
-const logoutBtn = document.getElementById("logoutBtn");
-
-function start() {
-	scanProduct.style.display = "none";
-	payElement.style.display = "none";
-	productInfoElement.style.display = "none";
-	searchElement.style.display = "none";
-	shoppingCart.style.display = "none";
-	navbarElement.style.display = "none";
-
-
-	login()
-}
-
-
-// Login
-function login() {
-	const inputEmail = document.getElementById("inputEmail");
-	const inputPassword = document.getElementById("inputPassword");
-	const loginBtn = document.getElementById("loginBtn");
-
-
-	loginBtn.addEventListener("click", e => {
-		const email = inputEmail.value;
-		const pass = inputPassword.value;
-		const auth = firebase.auth();
-
-		// Sign in 
-		const promise = auth.signInWithEmailAndPassword(email, pass);
-		promise.catch(e => console.log(e.message));
-
-
-	})
-}
-
-// OnstateChanged
-firebase.auth().onAuthStateChanged(firebaseUser => {
-	if (firebaseUser) {
-		console.log(firebaseUser);
-		enterApp()
-
-
-	} else {
-		console.log("not logged in");
-	}
-})
-
-// Logout eventlistener
-logoutBtn.addEventListener("click", e => {
-	firebase.auth().signOut();
-	loginPage()
-})
-
-function enterApp() {
-=======
 }
 function signUpUser(){
 	this.emailIn = document.getElementById("inputEmailSignUp").value
@@ -123,47 +62,15 @@ function signUpUser(){
 		})
 }
 function searchPage(){
->>>>>>> 579a9cf62d8c4e85f337e6dfc7be3ac185b02857
 	searchElement.style.display = "block";
-	loginElement.style.display = "none"
-	navbarElement.style.display = "flex";
-
-}
-
-function loginPage() {
-	searchElement.style.display = "none";
-	printSearch.style.display = "none";
 	scanElement.style.display = "none";
 	payElement.style.display = "none";
 	cartElement.style.display = "none";
-	printCart.style.display = "none";
-	loginElement.style.display = "block"
-	navbarElement.style.display = "none";
 	productInfoElement.style.display = "none";
-<<<<<<< HEAD
-	printproductInfo.style.display = "none";
-=======
 	signInElement.style.display = "none"
 	signUpElement.style.display = "none"
 	navBarElement.style.display = "block";
 
-	var pressInfo = new PressInfo(model, printproductInfo)
->>>>>>> 579a9cf62d8c4e85f337e6dfc7be3ac185b02857
-
-}
-
-function searchPage() {
-=======
-function searchPage(){
->>>>>>> parent of 42b7e5a... login
-=======
-function searchPage(){
->>>>>>> parent of 42b7e5a... login
-	searchElement.style.display = "block";
-	scanElement.style.display = "none";
-	payElement.style.display = "none";
-	cartElement.style.display = "none";
-	productInfoElement.style.display = "none";
 	var pressInfo = new PressInfo(model, printproductInfo)
 
 }
@@ -206,22 +113,7 @@ function cartPage(){
 	navBarElement.style.display = "block";
 
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-function productInfoPage(g) {
-	console.log("add vara: " + g)
-=======
 function productInfoPage(g){
->>>>>>> 579a9cf62d8c4e85f337e6dfc7be3ac185b02857
-=======
-function productInfoPage(g){
-	console.log("add vara: "+ g)
->>>>>>> parent of 42b7e5a... login
-=======
-function productInfoPage(g){
-	console.log("add vara: "+ g)
->>>>>>> parent of 42b7e5a... login
 	model.setCurrentProduct(g);
 
 	searchElement.style.display = "none";
@@ -234,30 +126,6 @@ function productInfoPage(g){
 	navBarElement.style.display = "block";
 
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-//scanPage();
-start()
-
-
-
-
-
-=======
-scanPage();
->>>>>>> parent of 42b7e5a... login
-=======
-scanPage();
->>>>>>> parent of 42b7e5a... login
-//
-// 	var searchElement = document.getElementById("search.html");
-//
-//   var search = new Search(model, searchElement);
-//   var shoppingCart = new ShoppingCart(model);
-//   var pressInfo = new PressInfo(model);
-=======
->>>>>>> 579a9cf62d8c4e85f337e6dfc7be3ac185b02857
 
 signUpPage();
 
