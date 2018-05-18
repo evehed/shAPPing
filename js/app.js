579a9cf62d8c4e85f337e6dfc7be3ac185b02857
evehed <evehed@kth.se>
2018-05-18 12:09:52 +0200

    fix sign in

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyDFN8hxCYuOipmB-UWY0JsQPICV6jSqxHM",
+	authDomain: "shapping-41333.firebaseapp.com",
+	databaseURL: "https://shapping-41333.firebaseio.com",
+	projectId: "shapping-41333",
+	storageBucket: "shapping-41333.appspot.com",
+	messagingSenderId: "987966090005"
+};
+firebase.initializeApp(config);
+//this.database = firebase.firestore();
+
 //We instantiate our model
 this.model = new ShoppingModel();
-
 
 var searchElement = document.getElementById("search.html");
 var printSearch= document.getElementById("search");
@@ -13,13 +24,54 @@ var loginElement = document.getElementById("login.html")
 var defaultElement = document.getElementById("default-page")
 var productInfoElement = document.getElementById("productInfo.html")
 var printproductInfo = document.getElementById("productInfo")
+var signInElement = document.getElementById("logIn.html")
+var signUpElement = document.getElementById("signUp.html")
+var navBarElement = document.getElementById("navBar.html")
 
+function signUpPage(){
+	console.log("signUp")
+	searchElement.style.display = "none";
+	scanElement.style.display = "none";
+	payElement.style.display = "none";
+	cartElement.style.display = "none";
+	productInfoElement.style.display = "none";
+	signInElement.style.display = "none";
+	signUpElement.style.display = "block";
+	navBarElement.style.display = "none";
+
+}
+function signUpUser(){
+	this.emailIn = document.getElementById("inputEmailSignUp").value
+	this.passwordIn = document.getElementById("inputPasswordSignUp").value
+
+	//Create new user with firebase
+	firebase.auth().createUserWithEmailAndPassword(this.emailIn, this.passwordIn)
+	var currentUser = firebase.auth().currentUser
+
+	//Create a user instance in database for the created user
+		firebase.firestore().doc('users/'+ currentUser.uid).set({
+			email: currentUser.email,
+		})
+		firebase.firestore().doc('users/'+ currentUser.uid).collection('shoppingCart').add({
+			p1: "",
+			p2: "",
+			p3: "",
+			p4: "",
+		})
+		.catch((err) => {
+			 	console.log("Error: "+err);
+		})
+}
 function searchPage(){
 	searchElement.style.display = "block";
 	scanElement.style.display = "none";
 	payElement.style.display = "none";
 	cartElement.style.display = "none";
 	productInfoElement.style.display = "none";
+	signInElement.style.display = "none"
+	signUpElement.style.display = "none"
+	navBarElement.style.display = "block";
+
 	var pressInfo = new PressInfo(model, printproductInfo)
 
 }
@@ -33,6 +85,10 @@ function scanPage(){
 	payElement.style.display = "none";
 	cartElement.style.display = "none";
 	productInfoElement.style.display = "none";
+	signInElement.style.display = "none"
+	signUpElement.style.display = "none"
+	navBarElement.style.display = "block";
+
 }
 
 function payPage(){
@@ -41,6 +97,10 @@ function payPage(){
 	payElement.style.display = "block";
 	cartElement.style.display = "none";
 	productInfoElement.style.display = "none";
+	signInElement.style.display = "none"
+	signUpElement.style.display = "none"
+	navBarElement.style.display = "block";
+
 
 }
 function cartPage(){
@@ -49,9 +109,12 @@ function cartPage(){
 	payElement.style.display = "none";
 	cartElement.style.display = "block";
 	productInfoElement.style.display = "none";
+	signInElement.style.display = "none"
+	signUpElement.style.display = "none"
+	navBarElement.style.display = "block";
+
 }
 function productInfoPage(g){
-	console.log("add vara: "+ g)
 	model.setCurrentProduct(g);
 
 	searchElement.style.display = "none";
@@ -59,16 +122,13 @@ function productInfoPage(g){
 	payElement.style.display = "none";
 	cartElement.style.display = "none";
 	productInfoElement.style.display = "block";
+	signInElement.style.display = "none"
+	signUpElement.style.display = "none"
+	navBarElement.style.display = "block";
 
 }
-scanPage();
-//
-// 	var searchElement = document.getElementById("search.html");
-//
-//   var search = new Search(model, searchElement);
-//   var shoppingCart = new ShoppingCart(model);
-//   var pressInfo = new PressInfo(model);
 
+signUpPage();
 
 // var beaconApp = new BeaconApp(model);
 
